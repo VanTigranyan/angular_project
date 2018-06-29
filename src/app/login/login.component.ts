2f0e7c15c3483d26e5f526c84c62422a62982585
VanTigranyan <lostfank@gmail.com>
2018-06-29 17:51:39 +0400

    Changed Data Service functions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { UserDataService } from '../user-data.service';
-import { Router } from '@angular/router';
+import {UserDataService} from '../user-data.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,29 +10,34 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private userService: UserDataService, private router: Router) { }
+  constructor(private userService: UserDataService, private router: Router) {
+  }
+
   form: FormGroup;
+
   onSubmit() {
-    this.userService.getUserData();
-    if (this.userService.findUserName(this.form.controls.userName)) {
-      if (this.userService.findUser(this.form.controls.userName).password === this.form.controls.password) {
-        this.router.navigate(['/user-page'],
-          {queryParams: {
-                userName: this.form.controls.userName
-          }});
+    if (this.userService.findUserName(this.form.controls.userName.value)) {
+      if (this.userService.findUser(this.form.controls.userName.value).password === this.form.controls.password.value) {
+        this.router.navigate(
+          ['/user-page'],
+            {
+              queryParams: {
+                userName: this.form.controls.userName.value
+              }
+            }
+        );
       }
     }
   }
 
   ngOnInit() {
     this.form = new FormGroup({
-      userName : new FormControl('', [
+      userName: new FormControl('', [
         Validators.required,
         Validators.minLength(6)]),
       password: new FormControl('', [
         Validators.required,
-        Validators.minLength(4),
-        Validators.pattern('^(?=.*\\d)(?=.*[a-zA-Z]).{6,15}$')
+        Validators.minLength(4)
       ])
     });
   }
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {AfterViewInit, Injectable, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
-export class UserDataService {
+export class UserDataService implements AfterViewInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.getUserData();
+  }
+
   userInfoArray;
 
   getUserData() {
@@ -12,30 +15,40 @@ export class UserDataService {
       .subscribe(data => this.userInfoArray = data);
   }
 
-  addUserData(name: string, lastName: string, userName: string, email: string, age: number) {
+  addUserData(name: string, lastName: string, userName: string, password: string, email: string, age: number) {
     this.userInfoArray.push({
       name: name,
       lastName: lastName,
       userName: userName,
+      password: password,
       email: email,
       age: age
     });
   }
 
 
+
+  findUser(userName) {
+    for (let i = 0; i > this.userInfoArray.length; i++) {
+      if (this.userInfoArray[i].userName === userName) {
+        return this.userInfoArray[i];
+      }
+    }
+  }
+
+
   findUserName(userName) {
-    for (const item of this.userInfoArray) {
-      if (item[userName] === userName) {
+    console.log(userName, this.userInfoArray);
+    for (let i = 0; i > this.userInfoArray.length; i++) {
+      if (this.userInfoArray[i].userName === userName){
         return true;
       }
-      return false;
     }
   }
-  findUser(userName) {
-    for (const item of this.userInfoArray){
-      if (item[userName] === userName){
-        return item;
-      }
+
+
+    ngAfterViewInit() {
     }
-  }
+
 }
+
